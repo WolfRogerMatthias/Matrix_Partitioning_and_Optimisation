d91aba51ee53fe78cd9d88bfa50e010fc93d5f8b
Roger Wolf <8mr66j28ff@privaterelay.appleid.com>
2025-02-28 13:51:44 +0100

    added Matrix Reader class with 3 function one for laoding the h5 file and one for printing the loaded matirces and one for sub_matrixes to print

---
+++ Code/MatrixReader.py
@@ -0,0 +1,31 @@
+import numpy as np
+import h5py
+
+class MatrixReader:
+    def __init__(self):
+        self.matrix = []
+
+    def load_h5_file(self, filename, num_matrices):
+        with h5py.File(filename, 'r') as ffile:
+            self.matrix = {i: np.array(ffile[f'cost_matrix_{i}']) for i in range(num_matrices)}
+        return self.matrix
+
+    def print_matrix(self, pos):
+        row, col = self.matrix[pos].shape
+
+        outputstr = ''
+        for i in range(row):
+            for j in range(col):
+                outputstr += str('{:.3f}'.format(round(self.matrix[pos][i, j], 3))) + ' '
+            outputstr += '\n'
+        print(outputstr)
+
+    def print_sub_matrix(self, matrix):
+        row, col = matrix.shape
+        outputstr = ''
+        for i in range(row):
+            for j in range(col):
+                outputstr += str('{:.3f}'.format(round(matrix[i, j], 3))) + ' '
+            outputstr += '\n'
+        print(outputstr)
+
