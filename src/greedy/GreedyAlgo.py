1734f54c06f1a821664da61d7a06f779daa75f69
Roger Wolf <roger.wolf@students.unibe.ch>
2025-03-11 17:41:11 +0100

    update the linear sum assignment and applied it to the greedy alog

--- src/greedy/GreedyAlgo.py
+++ src/greedy/GreedyAlgo.py
@@ -1,6 +1,8 @@
 import numpy as np
 from src.MatrixReader import MatrixReader
 from itertools import combinations
+from itertools import chain
+from src.optimize.OptimizeAlgoApplied import OptimizeAlgoApplied
 
 class GreedyAlgo:
 
@@ -16,6 +18,9 @@ class GreedyAlgo:
 if __name__ == "__main__":
     matrix_reader = MatrixReader()
     greedy_algo = GreedyAlgo()
+    applied_liner_sum = OptimizeAlgoApplied()
+
+    num_of_matrix = 0
 
     len_dataset = 188
     num_cost_matrices = len(list(combinations(range(len_dataset), r=2)))
@@ -24,13 +29,49 @@ if __name__ == "__main__":
 
     cost_matrices = matrix_reader.load_h5_file('../data/cost_matrices.h5', num_cost_matrices)
 
-    matrix_reader.print_matrix(10000)
+    matrix_reader.print_matrix(num_of_matrix)
     sub_matrices = []
+    row_intervals = []
+    col_intervals = []
     for i in range(num_cost_matrices):
         rows = len(cost_matrices[i])
         cols = len(cost_matrices[i][0])
-        sub_matrices.append(greedy_algo.greedy_sub_matrices(cost_matrices[i], [round(rows / 4), round(rows / 2), round(rows / 4) * 3], [round(cols / 4), round(cols / 2), round(cols / 4) * 3]))
+        row_interval = [round(rows / 4), round(rows / 2), round(rows / 4) * 3]
+        col_interval = [round(cols / 4), round(cols / 2), round(cols / 4) * 3]
+        row_intervals.append(row_interval)
+        col_intervals.append(col_interval)
+        sub_matrices.append(greedy_algo.greedy_sub_matrices(cost_matrices[i], row_interval, col_interval))
 
-    for i in range(len(sub_matrices[10000])):
-        matrix_reader.print_sub_matrix(sub_matrices[10000][i])
+    for i in range(len(sub_matrices[num_of_matrix])):
+        matrix_reader.print_sub_matrix(sub_matrices[num_of_matrix][i])
+
+    mapping_row = []
+    mapping_col = []
+    for i in range(len(sub_matrices[num_of_matrix])):
+        row, col = applied_liner_sum.compute_linear_sum_assignment(sub_matrices[num_of_matrix][i])
+        mapping_row.append(row)
+        mapping_col.append(col)
+
+    for i in range(len(mapping_row)):
+        applied_liner_sum.print_linear_sum_assignment_sub(i)
+
+    complete_row_mapping = []
+    complete_col_mapping = []
+    for i in range(len(mapping_row)):
+        if (i < 1):
+            complete_row_mapping.append(mapping_row[i])
+            complete_col_mapping.append(mapping_col[i])
+        else:
+            col_len = col_intervals[num_of_matrix][i - 1]
+            row_len = row_intervals[num_of_matrix][i - 1]
+            complete_row_mapping.append((mapping_row[i] + row_len))
+            complete_col_mapping.append((mapping_col[i] + col_len))
+
+
+    total_mapping = [list(chain(*complete_row_mapping)), list(chain(*complete_col_mapping))]
+    applied_liner_sum.print_linear_sum_assignment(total_mapping)
+
+    test_row, test_col = applied_liner_sum.compute_linear_sum_assignment(cost_matrices[num_of_matrix])
+    test_map = [test_row, test_col]
+    applied_liner_sum.print_linear_sum_assignment(test_map)
 
--- src/optimize/OptimizeAlgoApplied.py
+++ src/optimize/OptimizeAlgoApplied.py
@@ -1,49 +1,22 @@
-import numpy
-from src.MatrixReader import MatrixReader
-from itertools import combinations
 from scipy.optimize import linear_sum_assignment
-from src.kmeans.KMeansAlgo import KMeansAlgo
+
 
 class OptimizeAlgoApplied:
-    def __init__(self, row_ind, col_ind):
-        row_ind, col_ind = row_ind, col_ind
+    def __init__(self):
+        self.row_ind, self.col_ind = [], []
 
-
-    def optimize_applied(self, matrix):
-        if not isinstance(matrix, numpy.ndarray):
-            return "Not injective", "Not injective"
+    def compute_linear_sum_assignment(self, matrix):
         row, col = linear_sum_assignment(matrix)
-        row_ind.append(row)
-        col_ind.append(col)
+        self.row_ind.append(row)
+        self.col_ind.append(col)
+        return row, col
 
-
-
-if __name__ == '__main__':
-    matrix_reader = MatrixReader()
-    kmeans_algo = KMeansAlgo()
-    row_ind, col_ind = [], []
-    optimize_applied = OptimizeAlgoApplied(row_ind, col_ind)
-
-    len_dataset = 188
-    num_cost_matrices = len(list(combinations(range(len_dataset), r=2)))
-
-    print(f'Number of cost matrices: {num_cost_matrices}')
-
-    cost_matrices = matrix_reader.load_h5_file('../data/cost_matrices.h5', num_cost_matrices)
-
-    matrix_reader.print_matrix(0)
-
-    sub_matrices = kmeans_algo.kmeans_sub_matrix(cost_matrices[0], 3)
-
-    for i in range(len(sub_matrices)):
-        matrix_reader.print_sub_matrix(sub_matrices[i])
-        optimize_applied.optimize_applied(sub_matrices[i])
-
-    for i in range(len(row_ind)):
-        if not isinstance(row_ind[i], numpy.ndarray):
-            print("Not injective")
-        else:
-            for j in range(len(row_ind[i])):
-                print(f'{row_ind[i][j]} -> {col_ind[i][j]}')
+    def print_linear_sum_assignment_sub(self, num_mapping):
+        for i in range(len(self.row_ind[num_mapping])):
+            print(f'{self.row_ind[num_mapping][i]} -> {self.col_ind[num_mapping][i]}')
         print('\n')
+
+    def print_linear_sum_assignment(self, mapping):
+        for i in range(len(mapping[0])):
+            print(f'{mapping[0][i]} -> {mapping[1][i]}')
 
