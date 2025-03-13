921e740eaf73aaaffaaeac78a5bb45095daf2599
Roger Wolf <roger.wolf@students.unibe.ch>
2025-03-13 13:42:19 +0100

    code cleanup

--- src/greedy/GreedyAlgo.py
+++ src/greedy/GreedyAlgo.py
@@ -9,10 +9,14 @@ class GreedyAlgo:
     def greedy_sub_matrices(self, matrix, vertical_split, horizontal_split):
         if not isinstance(vertical_split, list) or not isinstance(horizontal_split, list):
             return "Invalid input"  # Check if vertical and Horizontal are lists
-        vertical_slices = np.vsplit(matrix, vertical_split)
+
+        vertical_slices = np.vsplit(matrix, vertical_split) # does the vertical Split
+
         sub_matrices = []
-        for i in range(len(horizontal_split) + 1):
-            sub_matrices.append(np.hsplit(vertical_slices[i], horizontal_split)[i])
+        for i in range(len(horizontal_split) + 1): # for each vertical split do horizontal split
+            horizontal_slices = np.hsplit(vertical_slices[i], horizontal_split) # does the horizontal split
+            sub_matrices.append(horizontal_slices[i]) # adds de correct sub matrix to the return
+
         return sub_matrices
 
 if __name__ == "__main__":
@@ -42,22 +46,19 @@ if __name__ == "__main__":
         col_intervals.append(col_interval)
         sub_matrices.append(greedy_algo.greedy_sub_matrices(cost_matrices[i], row_interval, col_interval))
 
-    for i in range(len(sub_matrices[num_of_matrix])):
-        matrix_reader.print_sub_matrix(sub_matrices[num_of_matrix][i])
-
     mapping_row = []
     mapping_col = []
     for i in range(len(sub_matrices[num_of_matrix])):
+        matrix_reader.print_sub_matrix(sub_matrices[num_of_matrix][i])
         row, col = applied_liner_sum.compute_linear_sum_assignment(sub_matrices[num_of_matrix][i])
         mapping_row.append(row)
         mapping_col.append(col)
 
-    for i in range(len(mapping_row)):
-        applied_liner_sum.print_linear_sum_assignment_sub(i)
 
     complete_row_mapping = []
     complete_col_mapping = []
     for i in range(len(mapping_row)):
+        applied_liner_sum.print_linear_sum_assignment_sub(i)
         if (i < 1):
             complete_row_mapping.append(mapping_row[i])
             complete_col_mapping.append(mapping_col[i])
@@ -70,8 +71,3 @@ if __name__ == "__main__":
 
     total_mapping = [list(chain(*complete_row_mapping)), list(chain(*complete_col_mapping))]
     applied_liner_sum.print_linear_sum_assignment(total_mapping)
-
-    test_row, test_col = applied_liner_sum.compute_linear_sum_assignment(cost_matrices[num_of_matrix])
-    test_map = [test_row, test_col]
-    applied_liner_sum.print_linear_sum_assignment(test_map)
-
