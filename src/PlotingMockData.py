474a91e1e423048390ebc7ce956d595a35664740
Roger Wolf <roger.wolf@students.unibe.ch>
2025-04-09 10:55:03 +0200

    updating the ploting and then more timing runs and adding the greedy algo extended

--- src/PlotingMockData.py
+++ src/PlotingMockData.py
@@ -2,58 +2,59 @@ import matplotlib.pyplot as plt
 from src.MockDataGenerator import MockDataGenerator
 from src.OptimizeAlgoApplied import OptimizeAlgoApplied
 from src.GreedyAlgo import GreedyAlgo
+from src.GreedyAlgoExtended import GreedyAlgoExtended
 import os
 import timeit
+import time
+import h5py
 
 if __name__ == '__main__':
     MockDataGenerator = MockDataGenerator()
     OptimizeAlgoApplied = OptimizeAlgoApplied()
     GreedyAlgo = GreedyAlgo(OptimizeAlgoApplied)
-    """
-    matrix_sizes = [10, 20, 40, 60, 80, 100]
-    number_of_matrices = 10000
+    GreedyAlgoExtended = GreedyAlgoExtended(OptimizeAlgoApplied)
+
+
+    matrix_sizes = [i for i in range(10, 121, 5)]
+    num_of_matrices = 25000
 
     execution_times_linear = []
     execution_times_greedy = []
-
-    for matrix_size in matrix_sizes:
-        cost_matrices = MockDataGenerator.loadig_mock_data(f'./data/mock_data_{matrix_size}.h5', number_of_matrices)
-        execution_time_linear = timeit.timeit('[OptimizeAlgoApplied.compute_linear_sum_assignment(cost_matrices[i]) for i in range(number_of_matrices)]', globals=globals(), number=1)
-        execution_times_linear.append(execution_time_linear)
-        execution_time_greedy = timeit.timeit('GreedyAlgo.greedy_linear_applied(cost_matrices, 10000)', globals=globals(), number=1)
-        execution_times_greedy.append(execution_time_greedy)
+    execution_times_greedy_extended = []
 
     print(matrix_sizes)
-    print(execution_times_linear)
-    print(execution_times_greedy)
-
-    plt.plot(matrix_sizes, execution_times_linear, color='red')
-    plt.plot(matrix_sizes, execution_times_greedy, color='blue')
-    plt.xlim(min(matrix_sizes), max(matrix_sizes))
-    #plt.ylim(min(execution_times_linear + execution_times_greedy), max(execution_times_linear + execution_times_greedy))
-    plt.savefig(f'./png/mock_data_together.png', format='png')
-    plt.show()
-    """
-
-    matrix_sizes = [i for i in range(10, 41, 10)]
-    num_of_matrices = 10000
-
-    execution_times_linear = []
-
-    print(matrix_sizes)
+    start = time.time()
     for matrix_size in matrix_sizes:
         filepath = f'./data/mock_data_{matrix_size}.h5'
         MockDataGenerator.creating_mock_data(filepath, num_of_matrices, matrix_size)
         cost_matrices = MockDataGenerator.loadig_mock_data(filepath, num_of_matrices)
         execution_time_linear = timeit.timeit('[OptimizeAlgoApplied.compute_linear_sum_assignment(cost_matrices[i]) for i in range(num_of_matrices)]', globals=globals(), number=1)
         execution_times_linear.append(execution_time_linear)
+        execution_time_greedy = timeit.timeit('GreedyAlgo.greedy_linear_applied(cost_matrices, num_of_matrices)', globals=globals(), number=1)
+        execution_times_greedy.append(execution_time_greedy)
+        execution_time_greedy_extended = timeit.timeit('GreedyAlgoExtended.greedy_linear_applied(cost_matrices, num_of_matrices)', globals=globals(), number=1)
+        execution_times_greedy_extended.append(execution_time_greedy_extended)
         os.remove(filepath)
-
+    end = time.time()
+    print(f'Execution time: {end - start} seconds')
     plt.plot(matrix_sizes, execution_times_linear, color='red')
+    plt.plot(matrix_sizes, execution_times_greedy, color='blue')
+    plt.plot(matrix_sizes, execution_times_greedy_extended, color='green')
     plt.xlim(min(matrix_sizes), max(matrix_sizes))
-    plt.savefig(f'./png/mock_data_linear_{num_of_matrices}_{matrix_sizes[-1]}.png', format='png')
-    plt.title('Linear Sum Assignment for matrix size 10000')
+    plt.xticks(matrix_sizes, rotation=90)
+    plt.title(f'For number of matrices {num_of_matrices}')
     plt.xlabel('Matrix size')
+    plt.legend(['Linear Sum Assignment', 'Greedy Assignment', 'Greedy Extended Assignment'])
+    plt.savefig(f'./png/mock_data_linear_vs_greedy_vs_extended_{num_of_matrices}_{matrix_sizes[-1]}.png', format='png')
     plt.show()
+
+    with h5py.File(f'./data/timings_{num_of_matrices}_{matrix_sizes[-1]}.h5', 'w') as file:
+        file.create_dataset('linear_sum_assignment_timing', data=execution_times_linear)
+        file.create_dataset('greedy_sum_assignment_timing', data=execution_times_greedy)
+        file.create_dataset('greedy_extended_sum_assignment_timing', data=execution_times_greedy_extended)
+        file.create_dataset('matrix_sizes', data=matrix_sizes)
+        file.close()
+
+
 
 
